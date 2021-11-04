c52131d31200b41a0ba07e079fdd761d13fc7ad5
YouManh <92284579+YouVanManh@users.noreply.github.com>
2021-11-04 11:10:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Duyên';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Crush Của Mình Sinh Nhật Vui Vẻ ạ:<';
+const eventName = 'Chúc Crush Sinh Nhật Vui Vẻ ạ:<';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc người con gái tớ yêu tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc crush tớ yêu tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
